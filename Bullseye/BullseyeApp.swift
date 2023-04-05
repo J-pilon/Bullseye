b0070aebb93cf84c5f5d9ca9e7ef75de8dda974b
Josiah <josiah.pilon@gmail.com>
2023-04-05 10:51:05 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Josiah Pilon on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
